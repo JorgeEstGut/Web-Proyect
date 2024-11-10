71725800b1d375e1070a56dd5063c762576e716a
JorgeEstGut <infdevwolfscodev@outlook.com>
2024-11-10 16:52:40 -0500

    Funciones al nav

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-addEventListener(click, )
+//Funciones menu hamburguesa
+let check = document.getElementById('menu');
+let icono = document.getElementById('menu-icon');
+let menuItems = document.querySelectorAll('.nav-ul_li');
+
+check.addEventListener("change", function () {
+    if (check.checked) {
+        icono.classList.replace("fa-bars", "fa-xmark");
+    } else {
+        icono.classList.replace("fa-xmark", "fa-bars");
+    }
+});
+
+menuItems.forEach(function(item) {
+    item.addEventListener('click', function() {
+        check.checked = false;
+        icono.classList.replace("fa-xmark", "fa-bars");
+    });
+});
